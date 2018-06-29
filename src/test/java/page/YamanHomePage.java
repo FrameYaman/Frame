3bbcea63dbe833962e848a6229a4f401b3978c44
Lucas Casanova <lucas.casanova@yaman.com.br>
2018-06-29 09:39:02 -0300

    Subindo um frame

---
+++ src/test/java/page/YamanHomePage.java
@@ -0,0 +1,13 @@
+package page;
+
+import suporte.BaseDSL;
+
+public class YamanHomePage{
+
+	private BaseDSL base = new BaseDSL();
+	
+	public void page() {
+		
+	}
+
+}
---
+++ src/test/java/test/YamanHomeTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+import page.YamanHomePage;
+
+public class YamanHomeTest{	
+	
+	YamanHomePage page = new YamanHomePage();	
+	
+	@Test
+	public void teste() {	
+		
+	}
+	
+	
+}
