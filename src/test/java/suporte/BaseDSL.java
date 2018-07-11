4044680dfa3702bdd1beff1b489a29637c32af9d
lucas.casanova <lucas.casanova@note_pe0355xa.yaman.br>
2018-07-11 08:42:32 -0300

    atualização

--- src/test/java/suporte/BaseDSL.java
+++ src/test/java/suporte/BaseDSL.java
@@ -12,17 +12,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-
 public class BaseDSL {
-	 
+
 	WebDriver driver = new ChromeDriver();
-	
-	
-/******************** INTERAÇÕES NAVEGADOR ********************/
-	
+
+	/******************** INTERAÇÕES NAVEGADOR ********************/
+
 	/**
-	 * Indicar a url para navegação
-	 * Exemplo: url("http://www.exemplourl.com.br");
+	 * Indicar a url para navegação Exemplo: url("http://www.exemplourl.com.br");
+	 * 
 	 * @author lucas.casanova
 	 * @param url
 	 */
@@ -30,333 +28,343 @@ public class BaseDSL {
 		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
+
 	/**
-	 * Executa rolagem da página.
-	 * Exemplo: rolarPagina("window.scrollBy(0,650)");
+	 * Executa rolagem da página. Exemplo: rolarPagina("window.scrollBy(0,650)");
+	 * 
 	 * @author lucas.casanova
-	 */	
-	public void rolarPagina(String rolagem)
-	{
-	    ((JavascriptExecutor)driver).executeScript(rolagem);
+	 */
+	public void rolarPagina(String rolagem) {
+		((JavascriptExecutor) driver).executeScript(rolagem);
 	}
-	
+
 	/**
-	 * Espera o tempo determinado por milisegundos para carregar. Utilziar números inteiros.
-	  Exemplo: 'esperaCarregar(tempo)'
-	  @param by
-	  @param valor
-	  @author guilherme.teixeira
-	 */			
-	public void esperaCarregar(int tempo)
-	{
+	 * Espera o tempo determinado por milisegundos para carregar. Utilziar números
+	 * inteiros. Exemplo: 'esperaCarregar(tempo)'
+	 * 
+	 * @param by
+	 * @param valor
+	 * @author guilherme.teixeira
+	 */
+	public void esperaCarregar(int tempo) {
 		driver.manage().timeouts().implicitlyWait(tempo, TimeUnit.SECONDS);
 	}
-		
+
+	/**
+	 * Saída / Finalização do sistema
+	 * 
+	 * @author lucas.casanova
+	 */
 	public void saida() {
 		driver.quit();
 	}
-	
-	
-/******************** TEXTFIELD E TEXTAREA ********************/
-	        	           
+
+	/******************** TEXTFIELD E TEXTAREA ********************/
+
 	/**
-	 * Limpa a máscara que existe no campo de preenchimento e escreve texto neste mesmo campo.
-	 * É necessário indicar a TAG referente. Exemplo: 'escreveTexto(By.id("Id") , "texto")'
+	 * Limpa a máscara que existe no campo de preenchimento e escreve texto neste
+	 * mesmo campo. É necessário indicar a TAG referente. Exemplo:
+	 * 'escreveTexto(By.id("Id") , "texto")'
+	 * 
 	 * @author lucas.casanova
-	 * @param by (utilizado para indicar a Tag)
-	 * @param texto (utilizado para indicar o texto)
-	 */       	    	
-	public void escreveTexto(By by, String texto)
-	{
-	    driver.findElement(by).clear();
-	    driver.findElement(by).sendKeys(texto);
-	}    	
-		    	
+	 * @param by
+	 *            (utilizado para indicar a Tag)
+	 * @param texto
+	 *            (utilizado para indicar o texto)
+	 */
+	public void escreveTexto(By by, String texto) {
+		driver.findElement(by).clear();
+		driver.findElement(by).sendKeys(texto);
+	}
+
 	/**
 	 * Encontra campo de preenchimento por Name, escreve texto neste mesmo campo.
 	 * Exemplo: 'encontraNameEscreveTexto("Name" , "texto")'
+	 * 
 	 * @author lucas.casanova
-	 * @param name (utilizado para indicar TAG name)
-	 * @param texto (utilizado para indicar o texto)
+	 * @param name
+	 *            (utilizado para indicar TAG name)
+	 * @param texto
+	 *            (utilizado para indicar o texto)
 	 */
-	public void encontraNameEscreveTexto(String name, String texto)
-	{
-	    escreveTexto(By.name(name), texto);
+	public void encontraNameEscreveTexto(String name, String texto) {
+		escreveTexto(By.name(name), texto);
 	}
-	
+
 	/**
-	 * Limpa a máscara que existe no campo de preenchimento, escreve texto neste mesmo campo e pressiona Enter.
-	 * É necessário indicar a TAG referente. Exemplo: 'EscreveEClicaEnter(By.Id("Id") , "texto")'
+	 * Limpa a máscara que existe no campo de preenchimento, escreve texto neste
+	 * mesmo campo e pressiona Enter. É necessário indicar a TAG referente. Exemplo:
+	 * 'EscreveEClicaEnter(By.Id("Id") , "texto")'
+	 * 
 	 * @author lucas.casanova
-	 * @param by 
+	 * @param by
 	 * @param texto
 	 */
-	public void escreveEClicaEnter(By by, String texto)
-	{
-	    escreveTexto(by, texto);
-	    driver.findElement(by).sendKeys(Keys.ENTER);
+	public void escreveEClicaEnter(By by, String texto) {
+		escreveTexto(by, texto);
+		driver.findElement(by).sendKeys(Keys.ENTER);
 	}
-	
+
 	/**
-	 * Encontra Tag Name e clica com Backspace.
-	 * Exemplo: 'clicaComBackspace("name")'
+	 * Encontra Tag Name e clica com Backspace. Exemplo: 'clicaComBackspace("name")'
+	 * 
 	 * @author lucas.casanova
 	 * @param name
 	 */
-	public void clicaNameComBackspace(String name)
-	{
-	    driver.findElement(By.name(name)).sendKeys(Keys.BACK_SPACE);
+	public void clicaNameComBackspace(String name) {
+		driver.findElement(By.name(name)).sendKeys(Keys.BACK_SPACE);
 	}
-	
+
 	/**
-	 * Encontra campo de preenchimento por ClassName, escreve texto neste mesmo campo e pressiona Enter.
-	 * Exemplo: 'encontraClassEnviaTextoEnter("ClassName" , "texto")'.
+	 * Encontra campo de preenchimento por ClassName, escreve texto neste mesmo
+	 * campo e pressiona Enter. Exemplo: 'encontraClassEnviaTextoEnter("ClassName" ,
+	 * "texto")'.
+	 * 
 	 * @author lucas.casanova
 	 * @param className
 	 * @param texto
 	 */
-	public void encontraClassEnviaTextoEnter(String className, String texto)
-	{
-	    escreveEClicaEnter(By.className(className), texto);
+	public void encontraClassEnviaTextoEnter(String className, String texto) {
+		escreveEClicaEnter(By.className(className), texto);
 	}
-	
+
 	/**
-	 * Limpa a máscara que existe no campo de preenchimento, escreve texto neste mesmo campo.
-	 * Este método chama comando 'ClassName'. 
-	 * Exemplo: 'escreverNaClasse("NomeDaClasse" , texto)'
+	 * Limpa a máscara que existe no campo de preenchimento, escreve texto neste
+	 * mesmo campo. Este método chama comando 'ClassName'. Exemplo:
+	 * 'escreverNaClasse("NomeDaClasse" , texto)'
+	 * 
 	 * @param nome_classe
 	 * @param texto
 	 */
-	public void escreveNaClasse(String nome_classe, String texto)
-	{
-	    escreveTexto(By.className(nome_classe), texto);
+	public void escreveNaClasse(String nome_classe, String texto) {
+		escreveTexto(By.className(nome_classe), texto);
 	}
-	
+
 	/**
-	 * Limpa a máscara que existe no campo de preenchimento, escreve texto neste mesmo campo.
-	 * Este método chama comando 'Id'. 
-	 * Exemplo: 'escreveId("Id" , texto)'
+	 * Limpa a máscara que existe no campo de preenchimento, escreve texto neste
+	 * mesmo campo. Este método chama comando 'Id'. Exemplo: 'escreveId("Id" ,
+	 * texto)'
+	 * 
 	 * @author lucas.casanova
 	 * @param id_campo
 	 * @param texto
 	 */
-	public void escreveId(String id_campo, String texto)
-	{
-	    escreveTexto(By.id(id_campo), texto);
+	public void escreveId(String id_campo, String texto) {
+		escreveTexto(By.id(id_campo), texto);
 	}
-	
+
 	/**
 	 * Encontra na página da Web através de Id e reserva por Tag encontrada.
 	 * Exemplo: 'obterValorCampoAttribute("Id", "innerText")'
+	 * 
 	 * @author lucas.casanova
 	 * @param id_campo
 	 * @return
 	 */
-	public String obterValorCampoAttribute(By by , String tag)
-	{
-	    return driver.findElement(by).getAttribute(tag);
-	}	        
-	
+	public String obterValorCampoAttribute(By by, String tag) {
+		return driver.findElement(by).getAttribute(tag);
+	}
+
 	/**
 	 * Encontra na página da Web através de Id e reserva o texto encontrado.
 	 * Exemplo: 'obterCampoTexto("id")'
+	 * 
 	 * @author lucas.casanova
 	 * @param by
 	 * @return
 	 */
-	public String obterCampoTexto(By by)
-	{
-	    return driver.findElement(by).getText();
+	public String obterCampoTexto(By by) {
+		return driver.findElement(by).getText();
 	}
-	
+
 	/**
-	 * Verifica se campo obrigatório foi preenchido corretamente.
-	 * Exemplo: 'checarCampoObrigatorio("texto" , "id")'
+	 * Verifica se campo obrigatório foi preenchido corretamente. Exemplo:
+	 * 'checarCampoObrigatorio("texto" , "id")'
+	 * 
 	 * @author lucas.casanova
 	 * @param texto
 	 * @param by
 	 */
-	public void checarCampoObrigatorio(String texto, By by)
-	{
+	public void checarCampoObrigatorio(String texto, By by) {
 		Assert.assertEquals(texto, obterCampoTexto(by));
 	}
-			
-/******************** RADIO E CHECK ********************/	    
-	
-	/**
-	 * Encontra elemento pela indicação solicitada e clica.
-	 * @param by (utilizado para indicar a Tag)
-	 * É necessário indicar a TAG referente. 
-	 * Exemplo: 'common(By.Id("Id"))'
-	 * @author guilherme.teixeira
-	 */
-	public void common(By by)
-	{
-	    driver.findElement(by).click();
-	}
-	
-	/**
-	 * Encontra elemento e confirma se está selecionado.
-	 * Exemplo: 'isElementoMarcado(By.Id("Id"))'
-	 * @author guilherme.teixeira
-	 * @param by (utilizado para indicar a Tag)
-	 * @return Boolean
-	 */	
-	public Boolean isElementoMarcado(By by)
-	{
-	    return driver.findElement(by).isSelected();
-	}
-	
-	/**
-	 * Encontra campo Check, seleciona pressionando Espaço para marcar.
-	 * Exemplo: 'isCheckMarcadoComEspaco(By.Id("Id"))'
-	 * @param by (utilizado para indicar a Tag)
-	 * @author guilherme.teixeira
-	 */
-	public void isCheckMarcadoComEspaco(By by)
-	{
-	    driver.findElement(by).sendKeys(Keys.SPACE);
-	}
-	
-/******************** COMBO ********************/
+
+	/******************** RADIO E CHECK ********************/
 
 	/**
-	 * Encontra campo Combo e seleciona elemento por texto deste Combo.
-	 * Exemplo: 'selecionarCombo("Id" , "TextoDoCombo")'
+	 * Encontra elemento pela indicação solicitada e clica.
+	 * 
+	 * @param by
+	 *            (utilizado para indicar a Tag) É necessário indicar a TAG
+	 *            referente. Exemplo: 'common(By.Id("Id"))'
+	 * @author guilherme.teixeira
+	 */
+	public void common(By by) {
+		driver.findElement(by).click();
+	}
+
+	/**
+	 * Encontra elemento e confirma se está selecionado. Exemplo:
+	 * 'isElementoMarcado(By.Id("Id"))'
+	 * 
+	 * @author guilherme.teixeira
+	 * @param by
+	 *            (utilizado para indicar a Tag)
+	 * @return Boolean
+	 */
+	public Boolean isElementoMarcado(By by) {
+		return driver.findElement(by).isSelected();
+	}
+
+	/**
+	 * Encontra campo Check, seleciona pressionando Espaço para marcar. Exemplo:
+	 * 'isCheckMarcadoComEspaco(By.Id("Id"))'
+	 * 
+	 * @param by
+	 *            (utilizado para indicar a Tag)
+	 * @author guilherme.teixeira
+	 */
+	public void isCheckMarcadoComEspaco(By by) {
+		driver.findElement(by).sendKeys(Keys.SPACE);
+	}
+
+	/******************** COMBO ********************/
+
+	/**
+	 * Encontra campo Combo e seleciona elemento por texto deste Combo. Exemplo:
+	 * 'selecionarCombo("Id" , "TextoDoCombo")'
+	 * 
 	 * @param by
 	 * @param texto
 	 * @author guilherme.teixeira
 	 */
-	public void selecionarCombo(By by, String texto)
-	{
+	public void selecionarCombo(By by, String texto) {
 		WebElement elemento = driver.findElement(by);
 		Select combo = new Select(elemento);
 		combo.selectByVisibleText(texto);
 	}
-	
+
 	/**
 	 * Encontra campo Combo por Id e retira a seleção do elemento deste Combo.
 	 * Exemplo: 'deselecionarCombo("Id" , "TextoDoCombo")'
+	 * 
 	 * @param by
 	 * @param valor
 	 * @author guilherme.teixeira
 	 */
-	public void deselecionarCombo(By by, String valor)
-	{
+	public void deselecionarCombo(By by, String valor) {
 		WebElement elemento = driver.findElement(by);
 		Select combo = new Select(elemento);
 		combo.deselectByVisibleText(valor);
 	}
-	
+
 	/******************** BOTÃO ********************/
-	
+
 	/**
-	 * Encontra elemento Botão por indicação selecionada e clica.
-	 * É necessário indicar a TAG referente. Exemplo: 'commonClicar(By.Id("Id"))'
+	 * Encontra elemento Botão por indicação selecionada e clica. É necessário
+	 * indicar a TAG referente. Exemplo: 'commonClicar(By.Id("Id"))'
+	 * 
 	 * @param by
 	 */
-	public void commonClicar(By by)
-	{
+	public void commonClicar(By by) {
 		driver.findElement(by).click();
 	}
-	
+
 	/******************** TEXTOS ********************/
-	
+
 	/**
 	 * Encontra elemento Text por indicação selecionada e reserva texto encontrado.
 	 * É necessário indicar a TAG referente. Exemplo: 'obterTexto(By.Id("Id"))'
+	 * 
 	 * @param by
 	 * @param valor
 	 * @author guilherme.teixeira
 	 */
-	public String obterTexto(By by)
-	{
+	public String obterTexto(By by) {
 		return driver.findElement(by).getText();
 	}
-	
+
 	/******************** ALERTS ********************/
-	
+
 	/**
-	 * Desvia a atenção para um Alert obtém texto. Aceita Alert e retorna texto deste Alert.
-	 * Exemplo: 'alertaObterTextoEAceita()'
+	 * Desvia a atenção para um Alert obtém texto. Aceita Alert e retorna texto
+	 * deste Alert. Exemplo: 'alertaObterTextoEAceita()'
+	 * 
 	 * @param by
 	 * @param valor
 	 * @author guilherme.teixeira
 	 */
-	public String alertaObterTextoEAceita()
-	{
+	public String alertaObterTextoEAceita() {
 		Alert alerta = driver.switchTo().alert();
 		String valor = alerta.getText();
 		alerta.accept();
 		return valor;
 	}
-	
+
 	/**
-	 *	Desvia a atenção para um Alert obtém texto. Rejeita Alert e retorna texto deste Alert.
-	 * Exemplo: 'alertaObterTextoENega()'
+	 * Desvia a atenção para um Alert obtém texto. Rejeita Alert e retorna texto
+	 * deste Alert. Exemplo: 'alertaObterTextoENega()'
+	 * 
 	 * @param by
 	 * @param valor
 	 * @author guilherme.teixeira
 	 */
-	public String AlertaObterTextoENega()
-	{
+	public String AlertaObterTextoENega() {
 		Alert alerta = driver.switchTo().alert();
 		String valor = alerta.getText();
 		alerta.dismiss();
 		return valor;
 	}
-	
+
 	/**
 	 * Desvia a atenção para um Alert escreve texto indicado e aceita Alert.
 	 * Exemplo: 'alertaEscrever("Texto")'
+	 * 
 	 * @param by
 	 * @param valor
 	 * @author guilherme.teixeira
 	 */
-	public void alertaEscrever(String texto)
-	{
+	public void alertaEscrever(String texto) {
 		Alert alerta = driver.switchTo().alert();
 		alerta.sendKeys(texto);
 		alerta.accept();
 	}
 
+	/******************* FRAMES E JANELAS *******************/
 
-/******************* FRAMES E JANELAS *******************/
-	
 	/**
-	 * Desvia a atenção para um Frame por Id indicado e entra.
-	  Exemplo: 'entrarFrame("Id")'
-	  @param by
-	  @param valor
-	  @author guilherme.teixeira
+	 * Desvia a atenção para um Frame por Id indicado e entra. Exemplo:
+	 * 'entrarFrame("Id")'
+	 * 
+	 * @param by
+	 * @param valor
+	 * @author guilherme.teixeira
 	 */
-	public void entrarFrame(String elemento)
-	{
+	public void entrarFrame(String elemento) {
 		driver.switchTo().frame(elemento);
 	}
-	
+
 	/**
-	 *  Sai do Frame e retorna para a página principal.
-	  Exemplo: 'SairFrame()'
-	  @param by
-	  @param valor
-	  @author guilherme.teixeira
+	 * Sai do Frame e retorna para a página principal. Exemplo: 'SairFrame()'
+	 * 
+	 * @param by
+	 * @param valor
+	 * @author guilherme.teixeira
 	 */
-	public void sairFrame()
-	{
+	public void sairFrame() {
 		driver.switchTo().defaultContent();
 	}
 
 	/**
-	 *  Desvia a atenção para uma janela por Id indicado.
-	  Exemplo: 'trocarJanela("elemento")'
-	  @param by
-	  @param valor
-	  @author guilherme.teixeira
+	 * Desvia a atenção para uma janela por Id indicado. Exemplo:
+	 * 'trocarJanela("elemento")'
+	 * 
+	 * @param by
+	 * @param valor
+	 * @author guilherme.teixeira
 	 */
-	public void trocarJanela(String elemento)
-	{
+	public void trocarJanela(String elemento) {
 		driver.switchTo().window(elemento);
 	}
-	
+
 }
