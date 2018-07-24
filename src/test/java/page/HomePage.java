606d455c1c4bab64d0a21b11370068c86ceb194d
Lucas <lucas.bonani@gmail.com>
2018-07-24 10:21:18 -0300

    Auto stash before merge of "master" and "origin/master"
    atualização base

--- src/test/java/page/HomePage.java
+++ src/test/java/page/HomePage.java
@@ -16,7 +16,7 @@ public class HomePage {
 	/********** BARRA DE PESQUISA **********/
 
 	public void lupaPesquisa() {
-		baseDSL.commonClicar(By.className("fa fa-search"));
+		baseDSL.clicaElemento(By.className("fa fa-search"));
 	}
 
 	public void barraPesquisa(String texto) {
@@ -24,33 +24,33 @@ public class HomePage {
 	}
 
 	public void fecharBarraPesquisa() {
-		baseDSL.commonClicar(By.className("search-off"));
+		baseDSL.clicaElemento(By.className("search-off"));
 	}
 
 	/*********** MENU **********/
 
 	public void menuHome() {
-		baseDSL.commonClicar(By.linkText("HOME"));
+		baseDSL.clicaElemento(By.linkText("HOME"));
 	}
 
 	/*********** BOXS INTERATIVOS **********/
 
 	public void clicaPerformaceEDisponibilidade() {
-		baseDSL.commonClicar(By.xpath(
+		baseDSL.clicaElemento(By.xpath(
 				"//div[@id='content-box-1']/div[1]//a[@href='http://yaman.com.br/performance-disponibilidade/']"));
 	}
 
 	public void clicaSegurancaDeAplicacoes() {
-		baseDSL.commonClicar(
+		baseDSL.clicaElemento(
 				By.xpath("//div[@id='content-box-1']/div[2]//a[@href='http://yaman.com.br/seguranca-de-aplicacoes/']"));
 	}
 
 	public void clicaQaTestes() {
-		baseDSL.commonClicar(By.xpath("//div[@id='content-box-1']/div[3]//a[@href='http://yaman.com.br/qa-testes/']"));
+		baseDSL.clicaElemento(By.xpath("//div[@id='content-box-1']/div[3]//a[@href='http://yaman.com.br/qa-testes/']"));
 	}
 
 	public void clicaPerDevOps() {
-		baseDSL.commonClicar(By.xpath("//div[@id='content-box-1']/div[4]//a[@href='http://yaman.com.br/devops/']"));
+		baseDSL.clicaElemento(By.xpath("//div[@id='content-box-1']/div[4]//a[@href='http://yaman.com.br/devops/']"));
 	}
 
 	/********** FIM *********/
